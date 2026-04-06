3fa8b75ad7c24b165389df77b9461ab1767a183e
NoChillPoseidon <travis.r.weisberg@gmail.com>
2026-04-05 21:55:16 -0400

    Implement stub Player class for testing purposes
    Add a stub Player class for testing when poke_env is not installed.
    
    Signed-off-by: NoChillPoseidon <travis.r.weisberg@gmail.com>

--- src/ml/training_players.py
+++ src/ml/training_players.py
@@ -7,7 +7,7 @@ import logging
 from typing import Any
 
 import random
-from poke_env.player import Player
+try:     from poke_env.player import Player     _POKE_ENV_OK = True except ImportError:  # pragma: no cover     _POKE_ENV_OK = False      class Player:  # type: ignore         """Stub so tests can import this module without poke_env installed."""         def __init__(self, *args: object, **kwargs: object) -> None:             pass          def choose_random_move(self, battle: object) -> object:             raise ImportError("poke_env is not installed")          def create_order(self, move: object) -> object:             raise ImportError("poke_env is not installed")
 
 log = logging.getLogger(__name__)
 
